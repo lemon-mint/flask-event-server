346812d050c1ed6c8f2d78ffaabd48ebe18569b1
lemon-mint <55233766+lemon-mint@users.noreply.github.com>
2020-11-05 16:47:44 +0900

    'init'

---
+++ app.py
@@ -0,0 +1,89 @@
+import json
+import secrets
+import time
+
+from flask import (Flask, Request, Response, flash, jsonify, redirect,
+                   render_template)
+from flask import request as f_req
+from flask import request as fr
+from flask import send_from_directory, session
+
+import sse
+
+app = Flask(__name__)
+
+msgq = sse.MQSSE()
+
+
+def make_event(data: dict, event="default") -> str:
+    msg = f'data: {json.dumps(data)}\n\n'
+    if event is not None:
+        msg = f'event: {event}\n{msg}'
+    return msg
+
+
+@app.route('/sse/listen/<ch>')
+def sse_abcd(ch):
+    def stream(ch):
+        messages = msgq.listen(ch)
+        yield ":ok\n\n"
+        while True:
+            msg = messages.get()
+            yield msg
+    resp = Response(stream(ch), mimetype='text/event-stream')
+    resp.headers['Access-Control-Allow-Origin'] = '*'
+    return resp
+
+
+@app.route('/sse/deploy/<ch>')
+def sse_deploy(ch):
+    try:
+        if fr.args.get('sub'):
+            event_type = int(fr.args.get('sub'))
+        else:
+            event_type = 9999
+        curtime = time.time()
+        eventid = secrets.token_hex(10)
+        eventname = 'default'
+        if fr.args.get('type') and 0 < len(str(fr.args.get('type'))) and len(str(fr.args.get('type'))) < 16:
+            eventname = str(fr.args.get('type'))
+        msg = make_event(
+            data={
+                'time': curtime,
+                'eventid': eventid,
+                'sub': event_type
+            },
+            event=eventname
+        )
+        msgq.deploy(msg, ch)
+        return jsonify(
+            {
+                'success': True,
+                'time': curtime,
+                'eventid': eventid,
+                'sub': event_type
+            }
+        ), 200
+    except:
+        pass
+    return jsonify(
+        {
+            'success': False
+        }
+    ), 500
+
+
+@app.route('/sse/test')
+def testpage():
+    return render_template('sse.html')
+
+
+if __name__ == "__main__":
+    from cheroot.wsgi import PathInfoDispatcher as WSGIPathInfoDispatcher
+    from cheroot.wsgi import Server as WSGIServer
+    my_app = WSGIPathInfoDispatcher({'/': app})
+    server = WSGIServer(('0.0.0.0', 32000), my_app)
+    try:
+        server.start()
+    except KeyboardInterrupt:
+        server.stop()
---
+++ sse.py
@@ -0,0 +1,19 @@
+import queue
+
+class MQSSE:
+
+    def __init__(self):
+        self.sse_queue = []
+
+    def listen(self,id):
+        msg_queue = queue.Queue(maxsize=10)
+        self.sse_queue.append({'queue': msg_queue,'id':id})
+        return msg_queue
+
+    def deploy(self, msg, id):
+        for i in reversed(range(len(self.sse_queue))):
+            try:
+                if self.sse_queue[i]['id'] == id:
+                    self.sse_queue[i]['queue'].put_nowait(msg)
+            except queue.Full:
+                del self.sse_queue[i]
